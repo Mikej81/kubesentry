7fdd82eb2fd51a4dd695220e1348fcab6604df5b
Mikej81 <13910667+Mikej81@users.noreply.github.com>
2024-09-23 12:18:59 -0400

    example

--- monitor.sh
+++ monitor.sh
@@ -8,16 +8,16 @@ while true; do
     MEM_TOTAL=$(grep MemTotal /host/proc/meminfo | awk '{print $2}')
     MEM_FREE=$(grep MemFree /host/proc/meminfo | awk '{print $2}')
     MEM_USED=$((MEM_TOTAL - MEM_FREE))
-    MEM_UTIL=$(awk "BEGIN {printf \"%.2f\", $MEM_USED/$MEM_TOTAL*100}")
+    #MEM_UTIL=$(awk "BEGIN {printf \"%.2f\", $MEM_USED/$MEM_TOTAL*100}")
+    MEM_UTIL=$(awk -v used="$MEM_USED" -v total="$MEM_TOTAL" 'BEGIN {printf "%.2f", used/total*100}')
+
 
     # Send SNMP trap
-    # snmptrap -v 2c -c $COMMUNITY_STRING $TRAP_RECEIVER "" \
-    #     private.enterprises.1.3.6.1.4.1.2021.10.1.3.1 s "$CPU_USAGE" \
-    #     private.enterprises.1.3.6.1.2.1.25.2.3.1.6.101 s "$MEM_USAGE"
 
     #snmptrap -v 2c -c $COMMUNITY_STRING $TRAP_RECEIVER "" .1.3.6.1.4.1.2021.11.10 "" 6 1 "" .1.3.6.1.4.1.2021.11.10 s "CPU Usage: $CPU_USAGE%" .1.3.6.1.4.1.2021.11.50 s "Memory Usage: $MEM_USAGE%"
 
-    snmptrap -v 2c -c $COMMUNITY_STRING $TRAP_RECEIVER "" .1.3.6.1.4.1.2021.10.1.3.1 .1.3.6.1.4.1.2021.10.1.3.1 s $CPU_USAGE .1.3.6.1.4.1.2021.4.6.0 i $MEM_UTIL
+    snmptrap -v 2c -c $COMMUNITY_STRING $TRAP_RECEIVER "" .1.3.6.1.4.1.2021.10.1.3.1 .1.3.6.1.4.1.2021.10.1.3.1 s $CPU_USAGE 
+    snmptrap -v 2c -c $COMMUNITY_STRING $TRAP_RECEIVER "" .1.3.6.1.4.1.2021.4.6.0 .1.3.6.1.4.1.2021.4.6.0 i $MEM_FREE
 
     # worked snmptrap -v 2c -c public 192.168.2.25 "" .1.3.6.1.4.1.2021.10.1.3.1 .1.3.6.1.4.1.2021.10.1.3.1 s 90
 
